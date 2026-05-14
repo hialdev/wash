04fbe67a2ef4fb7178d475278d020444734fb6d0
Hi, I am Alif <al.developer12@gmail.com>
2026-05-14 15:12:23 +0700

    fix: restore JSON key to 'created_by' and force DB column for seamless frontend integration

--- modules/cms/models/order_service.go
+++ modules/cms/models/order_service.go
@@ -35,7 +35,7 @@ type OrderServiceProcess struct {
 	ProcessType    *string          `json:"process_type" gorm:"type:varchar(50);not null"` // pickup, processing, delivery, done, other
 	Description    *string          `json:"description,omitempty" gorm:"type:text"`
 	Images         *string          `json:"images,omitempty" gorm:"type:text"` // JSON Array
-	CreatedByID    *uuid.UUID       `json:"created_by_id" gorm:"type:uuid"`
+	CreatedByID    *uuid.UUID       `json:"created_by" gorm:"column:created_by;type:uuid"`
 	Creator        *authModels.User `json:"creator,omitempty" gorm:"foreignKey:CreatedByID;references:ID"`
 }
 
